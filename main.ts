25dfc8129d68da5a0e486b543d15652be0d3d798
SHAYAN <147740430+shani967@users.noreply.github.com>
2024-01-09 16:30:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+import inquirer from'inquirer';
+ const rn = Math.floor(Math.random()*10)
+ const answers = await inquirer.prompt([
+    {
+    input:"number",
+    name:"answer",
+    message:"lets try your luck 😉\n choose a number between 1 to 10 "
+    }
+])
+
+if(answers.answer == rn){
+    console.log("you win 😁👍,the correct answer is ",rn )
+}
+else {
+    console.log('oops, the answer is incorrect😥');
+    console.log("better luck next time 👀")
+    console.log('the correct answer was', rn);
+}
